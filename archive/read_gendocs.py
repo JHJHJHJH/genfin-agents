763a3b0457218d167424d171537fd66ee69af461
Junhan <junhan.loh@podium.io>
2025-08-22 15:52:46 +0800

    add readme

--- archive/read_gendocs.py
+++ archive/read_gendocs.py
@@ -28,30 +28,3 @@ doc.close()
 
 # print(md_text)
 # .\myenv\Scripts\activate
-#shield = hospital plan - no BI, because generally only have public and private. publicly available data
-
-# Wholelife & term covers (SAME FOR BOTH) :
-# death
-# total permanent disability (TPD)
-# critical illness (early-severe stage) 
-# -death benefit table (main)
-# -ci table
-# -tpd table
-# -surrender value table (only wholelife )
-
-# Wholelife 
-# - low coverage, high premium. Has cash value if nothing happens (Will have surrender value table)
-# Term 
-# - high coverage, low premium. No cash value. 
-# No multiplier
-
-
-#Annuity
-#lifetime income product , e.g. put 3-5 years payment term, yield yearly income
-#-impt points
-#1. guarranteed income
-#2. total income 
-
-#Navigator - like syfe/stashaway
-#no BI
-
